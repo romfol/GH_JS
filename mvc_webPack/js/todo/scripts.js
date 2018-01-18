0fe61b2c161e35c7a36c5ec76884861c73148fcf
romfol <romanfoltyn1@gmail.com>
2018-01-18 16:39:03 +0200

    inter commit

---
+++ mvc_webPack/js/todo/scripts.js
@@ -0,0 +1,6 @@
+require('./controller/task.js');
+require('./model/taskModel.js');
+require('./model/task.js');
+require('./view/task.js');
+require('./view/taskAddForm.js');
+require('./index.js');
