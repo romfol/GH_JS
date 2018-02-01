9e46dcf44050a51abfefc76103e724b4f590a52b
romfol <romanfoltyn1@gmail.com>
2018-02-01 02:40:46 +0200

    css loader added

--- mvc_webPack/js/todo/index.js
+++
@@ -1,5 +0,0 @@
-import taskController from './controller/task';
-
-window.addEventListener('load', function () {
-    taskController(document.getElementById('todo-list'));
-});
--- mvc_webPack/webpack.config.js
+++ mvc_webPack/webpack.config.js
@@ -2,10 +2,13 @@ let HtmlWebpackPlugin = require('html-webpack-plugin');
 let path = require('path');
 
 module.exports = {
-    entry: './js/todo/index.js',
+    entry: './src/js/todo/index.js',
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+        rules: [{test:/\.css$/, use:['style-loader','css-loader']}]
     },
     plugins: [
         new HtmlWebpackPlugin({
